16f28ca6a71c6298e61142e30357ff02b19b6055
YogeshGovindaraju <99166617+YogeshGovindaraju@users.noreply.github.com>
2022-06-28 22:59:41 +0530

    Add files via upload
    Added testcases for 12 bit A2D converter integration.

--- TestChargingCurrentSensor.h
+++ TestChargingCurrentSensor.h
@@ -1,13 +1,47 @@
 #ifndef TESTCHARGINGCURRENTSENSOR
 #define TESTCHARGINGCURRENTSENSOR
 
+void testForEmpty12BitCurrentSensorReadings(ChargingCurrentSensor &chargingCurrentSensor, std::vector<int> &A2DConverterReadings, std::stringstream &expectedOutput)
+{
+    A2DConverterReadings = {};
+    expectedOutput.str("");
+    expectedOutput << "\nNo Current Samples are given!!!";
+    assert(chargingCurrentSensor.classifyCurrentRangeFromSensorReadings(A2DConverterReadings) == expectedOutput.str());
+}
+
+void testForError12BitCurrentSensorReadings(ChargingCurrentSensor &chargingCurrentSensor, std::vector<int> &A2DConverterReadings, std::stringstream &expectedOutput)
+{
+    A2DConverterReadings = {-1, 4095};
+    expectedOutput.str("");
+    expectedOutput << "\nNo Current Samples are given!!!";
+    assert(chargingCurrentSensor.classifyCurrentRangeFromSensorReadings(A2DConverterReadings) == expectedOutput.str());
+}
+
+void testForSingle12BitCurrentSensorReading(ChargingCurrentSensor &chargingCurrentSensor, std::vector<int> &A2DConverterReadings, std::stringstream &expectedOutput)
+{
+    A2DConverterReadings = {4094};
+    expectedOutput.str("");
+    expectedOutput << "\nRange, Readings\n10-10, 1";
+    assert(chargingCurrentSensor.classifyCurrentRangeFromSensorReadings(A2DConverterReadings) == expectedOutput.str());
+}
+
+void testFor12BitCurrentSensorReadings(ChargingCurrentSensor &chargingCurrentSensor, std::vector<int> &A2DConverterReadings, std::stringstream &expectedOutput)
+{
+    A2DConverterReadings = {0, 249, 987, 1234, 2566, 3625, 4000, 4094};
+    expectedOutput.str("");
+    expectedOutput << "\nRange, Readings\n0-3, 4\n6-6, 1\n9-10, 3";
+    assert(chargingCurrentSensor.classifyCurrentRangeFromSensorReadings(A2DConverterReadings) == expectedOutput.str());
+}
+
 void test12BitChargingCurrentSensor()
 {
     ChargingCurrentSensor chargingCurrentSensor(0, 10, 12);
-    std::vector<int> A2DConverterReadings{4094};
+    std::vector<int> A2DConverterReadings;
     std::stringstream expectedOutput;
-    expectedOutput << "\nRange, Readings\n10-10, 1";
-    assert(chargingCurrentSensor.classifyCurrentRangeFromSensorReadings(A2DConverterReadings) == expectedOutput.str());
+    testForEmpty12BitCurrentSensorReadings(chargingCurrentSensor, A2DConverterReadings, expectedOutput);
+    testForError12BitCurrentSensorReadings(chargingCurrentSensor, A2DConverterReadings, expectedOutput);
+    testForSingle12BitCurrentSensorReading(chargingCurrentSensor, A2DConverterReadings, expectedOutput);
+    testFor12BitCurrentSensorReadings(chargingCurrentSensor, A2DConverterReadings, expectedOutput);
 }
 
 #endif
--- TestCurrentSensor.h
+++ TestCurrentSensor.h
@@ -1,28 +1,28 @@
 #ifndef TESTCURRENTSENSOR
 #define TESTCURRENTSENSOR
 
-void testForEmptyA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+void testForEmpty12BitA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
 {
     A2DConverterReadings = {};
     currentSensorReadings = {};
     assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
 }
 
-void testForErrorA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+void testForError12BitA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
 {
     A2DConverterReadings = {-1, 4095};
     currentSensorReadings = {};
     assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
 }
 
-void testForSingleA2DConverterReading(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+void testForSingle12BitA2DConverterReading(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
 {
     A2DConverterReadings = {4094};
     currentSensorReadings = {10};
     assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
 }
 
-void testForA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+void testFor12BitA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
 {
     A2DConverterReadings = {0, 249, 987, 1234, 2566, 3625, 4000, 4094};
     currentSensorReadings = {0, 1, 2, 3, 6, 9, 10, 10};
@@ -34,10 +34,10 @@ void test12BitCurrentSensor()
     CurrentSensor currentSensor(0, 10, 12);
     std::vector<int> A2DConverterReadings;
     std::vector<int> currentSensorReadings;
-    testForEmptyA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
-    testForErrorA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
-    testForSingleA2DConverterReading(currentSensor, A2DConverterReadings, currentSensorReadings);
-    testForA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
+    testForEmpty12BitA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
+    testForError12BitA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
+    testForSingle12BitA2DConverterReading(currentSensor, A2DConverterReadings, currentSensorReadings);
+    testFor12BitA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
 }
 
 #endif
