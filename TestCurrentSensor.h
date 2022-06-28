06693aec73867c188f243af2151150689d4037b2
YogeshGovindaraju <99166617+YogeshGovindaraju@users.noreply.github.com>
2022-06-28 18:36:30 +0530

    Add files via upload
    Added Testcases for 12 bit A2D converter sensor.

--- TestCurrentSensor.h
+++ TestCurrentSensor.h
@@ -1,12 +1,43 @@
 #ifndef TESTCURRENTSENSOR
 #define TESTCURRENTSENSOR
 
+void testForEmptyA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+{
+    A2DConverterReadings = {};
+    currentSensorReadings = {};
+    assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
+}
+
+void testForErrorA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+{
+    A2DConverterReadings = {-1, 4095};
+    currentSensorReadings = {};
+    assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
+}
+
+void testForSingleA2DConverterReading(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+{
+    A2DConverterReadings = {4094};
+    currentSensorReadings = {10};
+    assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
+}
+
+void testForA2DConverterReadings(CurrentSensor &currentSensor, std::vector<int> &A2DConverterReadings, std::vector<int> &currentSensorReadings)
+{
+    A2DConverterReadings = {0, 249, 987, 1234, 2566, 3625, 4000, 4094};
+    currentSensorReadings = {0, 1, 2, 3, 6, 9, 10, 10};
+    assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
+}
+
 void test12BitCurrentSensor()
 {
     CurrentSensor currentSensor(0, 10, 12);
-    std::vector<int> A2DConverterReadings{4094};
-    std::vector<int> currentSensorReadings{10};
-    assert(currentSensor.calculateCurrentFromA2DConverterReadings(A2DConverterReadings) == currentSensorReadings);
+    std::vector<int> A2DConverterReadings;
+    std::vector<int> currentSensorReadings;
+    testForEmptyA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
+    testForErrorA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
+    testForSingleA2DConverterReading(currentSensor, A2DConverterReadings, currentSensorReadings);
+    testForA2DConverterReadings(currentSensor, A2DConverterReadings, currentSensorReadings);
 }
 
 #endif
